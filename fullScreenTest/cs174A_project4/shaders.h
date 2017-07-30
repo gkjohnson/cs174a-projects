c1ec9d9944f61caa9b016e566cc99c7fbfb66070
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-07-29 19:13:19 -0700

    Get full screen test running again

--- fullScreenTest/cs174A_project4/shaders.h
+++ fullScreenTest/cs174A_project4/shaders.h
@@ -1,5 +1,5 @@
 //Vertex Shader Source Code
-const char* VERTEX_SHADER_SOURCE="in vec4 vPosition;\
+const char* VERTEX_SHADER_SOURCE = "in vec4 vPosition;\
 									in vec4 vNormal;\
 									in vec4 vUV;\
 									\
@@ -15,13 +15,16 @@ const char* VERTEX_SHADER_SOURCE="in vec4 vPosition;\
 									vPosition=vPosition*model_trans;\
 									fNormal=vNormal;\
 									fNormal.w=0;\
+									fNormal=fNormal*model_trans;\
+									fNormal=normalize(fNormal);\
+									\
 									fPos=vPosition;\
 									fPos.w=0;\
 									fUV=vUV;\
 									gl_Position = vPosition*camera_trans;\
 									} ";
 //Fragment Shader Source Code
-const char* FRAGMENT_SHADER_SOURCE="uniform sampler2D diffuseMap;\
+const char* FRAGMENT_SHADER_SOURCE = "\
 								   \
 								  uniform vec4 diffuseColor;\
 								  uniform vec4 lightColor;\
@@ -37,19 +40,16 @@ const char* FRAGMENT_SHADER_SOURCE="uniform sampler2D diffuseMap;\
 								  \
 								  void main()\
 								  {\
-								  diffuseColor=texture2D(diffuseMap,fUV);\
-								  lightPos.w=0;\
-								  camPos.w=0;\
 								  vec4 lightVector=normalize(lightPos-fPos);\
 								  vec4 viewDir=normalize(camPos-fPos);\
 								  \
 								  vec4 amb=ambientColor;\
-								  vec4 difMult=clamp(dot(fNormal,lightVector),vec4(0,0,0,0),vec4(1,1,1,1));\
-								  vec4 specMult=pow(clamp(dot(fNormal,normalize(viewDir+lightVector)),vec4(0,0,0,0),vec4(1,1,1,1)),shininess);\
-								  if(shininess==0||dot(fNormal,lightVector)<0){specMult=0;}\
+								  float difMult=clamp(dot(fNormal,lightVector),0,1);\
+								  float specMult=pow(clamp(dot(fNormal,normalize(viewDir+lightVector)),0,1),shininess);\
+								  if(shininess==0||dot(fNormal,lightVector)<0){specMult=0.0;}\
 								  \
 								  gl_FragColor=amb+difMult*diffuseColor*lightColor+specMult*difMult*lightColor;\
-								  }";
+								  } \n";
 
 
 //TODO: create a gourand shader
